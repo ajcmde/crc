38171a31aaa767517d6c25b9c29e9e3575d5ccbb
Alexander Eggerer <134808561+ajcmde@users.noreply.github.com>
2023-09-26 10:44:31 +0200

    CRCdump (dumps CRC as C code)

--- CRC.h
+++ CRC.h
@@ -5,20 +5,14 @@
 typedef struct {
     uint8_t crcbits; //8, 12, 16, 32, 64
     uint64_t polynom; 
-    uint64_t polymask;
-    uint64_t polyMSBit;
     uint64_t init;
     uint64_t XOrOut;
     bool RefIn;
     bool RefOut;
-
-    union {
-        uint8_t ui8[1];
-        uint16_t ui16[1];
-        uint32_t ui32[1];
-        uint64_t ui64[1];
-    } polytable;
+    uint64_t polytable[256];
 } CRChandle_t;
 
 extern CRChandle_t *CRCCreate(uint8_t crcbits, uint64_t polynom, uint64_t init, bool RefIn, bool RefOut, uint64_t XOrOut);
 extern uint64_t CRC(CRChandle_t *CRChandle, uint8_t *buffer, size_t length);
+
+extern char *CRCdump(CRChandle_t *CRChandle);
