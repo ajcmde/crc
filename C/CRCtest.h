5a7de7b0b912fbcd9ba24252462ed86eaf40a461
Alexander Eggerer <134808561+ajcmde@users.noreply.github.com>
2024-02-02 23:53:03 +0100

    Create CRCtest.h
    moved from test directory by recreating the file

---
+++ C/CRCtest.h
@@ -0,0 +1,9 @@
+#include "CRC.h"
+
+#if defined(CONFIG_IDF_TARGET_ESP32)
+/**
+ * @brief Run Test CRC tests. Fail/ pass results will be logged on console
+ *
+ */
+extern void _CRCTest(void);
+#endif
