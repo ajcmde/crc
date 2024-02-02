c5d13a027ce889b529c4aa4bc69320b0898a2624
Alexander Eggerer <134808561+ajcmde@users.noreply.github.com>
2024-02-02 23:55:17 +0100

    Delete test directory
    moved to C folder

--- test/crc_test.h
+++
@@ -1,9 +0,0 @@
-#include "crc.h"
-
-#if defined(CONFIG_IDF_TARGET_ESP32)
-/**
- * @brief Run Test CRC tests. Fail/ pass results will be logged on console
- *
- */
-extern void _CRCTest(void);
-#endif
